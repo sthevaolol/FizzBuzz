b5deb57e58c367fdb5e8bfcdd037a5aec38a88ee
Sthevão Marques <102769861+sthevaolol@users.noreply.github.com>
2023-09-18 19:03:30 -0300

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 function num () {
-    for(var i = 0; i < 101; i++) {
+    for(var i = 1; i < 101; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             console.log("FizzBuzz")
         } else if (i % 3 == 0 && i % 5 != 0) {
